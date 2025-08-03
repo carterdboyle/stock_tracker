4507a87235fa70482622c066614bf72bbeb4182a
Carter Boyle <carterboyle@msn.com>
2025-08-03 20:32:19 -0300

    Add functionality to track a stock

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "my_portfolio", to: "users#my_portfolio"
   get "search_stock", to: "stocks#search"
-  resources :user_stocks
+
+  resources :user_stocks, only: [ :create ]
 end
