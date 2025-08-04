3cf5fddf40a0b503ff228f725064e395efd3cee0
Carter Boyle <carterboyle@msn.com>
2025-08-04 14:11:38 -0300

    Searching for a friend functionality started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
+  def search
+    if params[:friend].present?
+      @friends = current_user.friends
+      @friend = params[:friend]
+      if @friend
+        render 'users/my_friends'
+      else
+        flash[:alert] = "Could not find user"
+        redirect_to my_friends_path
+      end
+    else
+      flash[:alert] = "Please enter a friend name or email to search"
+      redirect_to my_friends_path
+    end
+  end
+
 end
