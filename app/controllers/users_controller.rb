93201f18398458526511c73f15bc64d60e1dcb63
Carter Boyle <carterboyle@msn.com>
2025-08-04 14:49:50 -0300

    Layout cleanup and search user functionality finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,16 @@ class UsersController < ApplicationController
   end
 
   def my_friends
-    @friends = current_user.friends
+    @tracked_friends = current_user.friends
   end
 
   def search
     if params[:friend].present?
-      @friends = current_user.friends
-      @friend = params[:friend]
-      if @friend
+      @tracked_friends = current_user.friends
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      
+      if @friends
         render 'users/my_friends'
       else
         flash[:alert] = "Could not find user"
