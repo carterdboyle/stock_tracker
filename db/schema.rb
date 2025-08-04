5885b94644ff855dc4409ecf70dfe0a931a158b8
Carter Boyle <carterboyle@msn.com>
2025-08-04 10:01:49 -0300

    Adding first name and last name to database and show it on UI
    Added in a full_name helper method to the user model as well

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_01_154608) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_04_122819) do
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
     t.string "name"
@@ -36,6 +36,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_01_154608) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
