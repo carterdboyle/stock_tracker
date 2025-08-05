755619aa029b5c3b225b72a7d2633ba26bfee66b
Carter Boyle <carterboyle@msn.com>
2025-08-05 12:23:49 -0300

    Follow user functionality implemented
    Also, fixed an error if there are no matches

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,8 @@ class User < ApplicationRecord
   def self.search(param)
     param.strip!
     to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
-    return nil unless to_send_back
-    to_send_back
+    return to_send_back unless to_send_back.blank?
+    return nil
   end
 
   def self.first_name_matches(param)
@@ -52,6 +52,11 @@ class User < ApplicationRecord
   end
 
   def except_current_user(users)
+    return if users.blank?
     users.reject { | user | user.id === self.id }
+  end
+
+  def not_friends_with?(id_of_friend)
+    !self.friends.where(id: id_of_friend).exists?
   end
 end
